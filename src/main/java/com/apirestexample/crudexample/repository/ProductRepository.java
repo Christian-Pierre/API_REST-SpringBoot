50255bda1f73b77717b78a80f2bde0e38f7bdd07
Pierre <chris.pierre1999@gmail.com>
2021-10-02 17:44:39 -0300

    Starting

---
+++ src/main/java/com/apirestexample/crudexample/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.apirestexample.crudexample.repository;
+
+import com.apirestexample.crudexample.entity.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+    Product findByName(String name);
+}
